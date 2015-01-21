c7f0ba09e28ef8ac0279f9c3a85a8dfe5d248ffd
Paolo Montano <Paolo.Montano@Gmail.com>
2015-01-21 11:24:15 -0800

    Guess controller added.

--- application/config/routes.php
+++ application/config/routes.php
@@ -44,6 +44,7 @@ $route['404_override'] = '';
 $route['sleep'] = "first/zzz";
 $route['lock/(:any)/(:any)'] = "welcome/shucks";
 $route['show/(:num)'] = "first/gimmie/$1";
+$route['dunno'] = 'guess';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
